6404bc49ebf3480839410dc483072cf53482ee28
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-08-22 09:08:55 +0200

    remove some SB leftovers

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { MantineProvider } from '@mantine/core';
 import AppShell from './components/AppShell/AppShell';
 import Header from './components/AppShell/Header';
 import CSSPolygoneEditor from './components/CSSPolygonEditor/CSSPolygonEditor';
-// @ts-ignore
-import data from './data-mock.json';
 
 function App() {
   const [opened, setOpened] = React.useState(false);
