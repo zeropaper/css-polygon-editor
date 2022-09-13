58730877be0b4e85d51e111f3c7b8ff8cdbd905b
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-09-13 09:18:53 +0200

    add support for no_header=true

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-import { MantineProvider, Paper } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import AppShell from './components/AppShell/AppShell';
 import Header from './components/AppShell/Header';
 import CSSPolygoneEditor from './components/CSSPolygonEditor/CSSPolygonEditor';
 
 function App() {
+  const noHeader = window.location.search.indexOf('no_header=true') > -1;
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <AppShell header={<Header />}>
+      <AppShell header={noHeader ? undefined : <Header />}>
         <CSSPolygoneEditor />
       </AppShell>
     </MantineProvider>
